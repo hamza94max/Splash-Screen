0b0b9885986bf9813024375a6936fcfd94231298
hamza94max <54688005+hamza94max@users.noreply.github.com>
2020-06-09 08:27:35 +0200

    update

--- app/src/main/java/com/example/freelance/MainActivity.java
+++ app/src/main/java/com/example/freelance/MainActivity.java
@@ -13,10 +13,6 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity  {
-RecyclerView rec;
-    public RecAdapter RecAdapter;
-    Button button;
-    ArrayList<items> items=new ArrayList<items>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,33 +21,6 @@ RecyclerView rec;
 
 
 
-
-
-        button=findViewById(R.id.buttonn);
-        rec=findViewById(R.id.rec) ;
-
-
-        items.add(new items("أذكار الصباح "));
-        items.add(new items("أذكار المساء"));
-        items.add(new items("أذكار الأذان "));
-        items.add(new items("أذكار الصلاه"));
-        items.add(new items("أذكار المسجد"));
-        items.add(new items("أذكار بعد الصلاه "));
-        items.add(new items("أذكار الوضوء"));
-        items.add(new items("أذكار المنزل"));
-        items.add(new items("أذكار الطعام"));
-        items.add(new items("أذكار النوم"));
-        items.add(new items("أذكار الاستيقاظ من النوم"));
-        items.add(new items("الادعيه القرأنيه "));
-        items.add(new items("أدعيه النبويه "));
-        items.add(new items("دعاء ختم القران الكريم"));
-        items.add(new items("فضل الدعاء"));
-        items.add(new items("الرقيه الشرعيه"));
-
-
-        rec.setLayoutManager(new LinearLayoutManager(this));
-       RecAdapter=new RecAdapter(this,items);
-        rec.setAdapter(RecAdapter);
 
     }
 
